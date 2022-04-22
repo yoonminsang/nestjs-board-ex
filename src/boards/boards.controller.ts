c3d240c2b9eeace12b7d0a9074289ebb9a62cf06
yoonminsnag <apdlyooapdl@naver.com>
2022-04-23 01:06:12 +0900

    feat: 모든 글 읽기 구현

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { Board } from './board.model';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+  constructor(private readonly boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+}
