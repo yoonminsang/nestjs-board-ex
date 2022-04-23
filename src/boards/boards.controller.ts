75987adee6d8fe172760efabf7f63ccf70a36fda
yoonminsnag <apdlyooapdl@naver.com>
2022-04-23 21:28:01 +0900

    feat: 커스텀 파이프

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -12,6 +12,7 @@ import {
 import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -39,7 +40,10 @@ export class BoardsController {
   }
 
   @Patch('/:id/status')
-  updateBoardStatus(@Param() id: string, @Body() status: BoardStatus) {
-    return this.updateBoardStatus(id, status);
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} isn't in the status options`);
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    return this.StatusOptions.includes(status);
+  }
+}
