86e89d58fe7a5de3c23b4a1f7022b9cc02410017
yoonminsnag <apdlyooapdl@naver.com>
2022-04-23 10:14:20 +0900

    feat: 특정 글 읽기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -15,5 +15,10 @@ export class BoardsController {
   @Post('/')
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param() id: string): Board {
+    return this.boardsService.getBoardById(id);
   }
 }
