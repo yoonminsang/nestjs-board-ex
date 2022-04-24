41cbb529af7652603fd14043718b53c34cae35f0
yoonminsnag <apdlyooapdl@naver.com>
2022-04-24 23:22:37 +0900

    feat: logger 적용

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   ParseIntPipe,
   Patch,
@@ -23,6 +24,7 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe'
 @Controller('boards')
 @UseGuards(AuthGuard('jwt'))
 export class BoardsController {
+  private logger = new Logger('BoardsController');
   constructor(private readonly boardsService: BoardsService) {}
 
   //   @Get('/')
@@ -38,6 +40,7 @@ export class BoardsController {
   getAllUserBoards(
     @Param('userId', ParseIntPipe) userId: number,
   ): Promise<Board[]> {
+    this.logger.verbose(`User ${userId} trying to get boards`);
     return this.boardsService.getAllUserBoards(userId);
   }
 
@@ -52,6 +55,11 @@ export class BoardsController {
     @Body() createBoardDto: CreateBoardDto,
     @GetUser() user: User,
   ): Promise<Board> {
+    this.logger.verbose(
+      `User ${user.username} creating a new board. Payload: ${JSON.stringify(
+        createBoardDto,
+      )}`,
+    );
     return this.boardsService.createBoard(createBoardDto, user);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(
+    `NODE_ENV is ${process.env.NODE_ENV}
+    Application running on port ${port}`,
+  );
 }
 bootstrap();
